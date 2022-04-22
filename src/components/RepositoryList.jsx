07c771f4ff764adc57a7bef00e19bc3244adbf4c
danielvalmeida91 <danielvalmeida91@gmail.com>
2022-04-22 12:25:16 -0300

    Initial configuration, babel and webpack

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,35 @@
+import {useState, useEffect} from 'react';
+import { RepositoryItem } from "./RepositoryItem";
+
+import '../styles/repositories.scss';
+
+//https://api.github.com/orgs/rocketseat/repos
+
+const repository = {
+    name: 'unform',
+    description: 'Forms in ReactJS',
+    link: 'https:/www.github.com/unform/unform'
+}
+
+export function RepositoryList(){
+    const [repositories, setRepositories] = useState([]);
+
+    useEffect(()=> {
+        fetch('https://api.github.com/orgs/rocketseat/repos')
+        .then(response => response.json())
+        .then(data => console.log(data))
+    },[]);
+    return(
+        <section className="repository-list">
+            <h1>Lista de Repositórios</h1>
+
+            <ul>
+               <RepositoryItem repository={repository}/> 
+               <RepositoryItem repository={repository}/> 
+               <RepositoryItem repository={repository}/> 
+               <RepositoryItem repository={repository}/> 
+            </ul>
+            
+        </section>
+    );
+}
